b9344daf2bcb43f3902cff7c105cb8e62ffb822f
pangboww <pangboww@gmail.com>
2015-05-25 16:32:53 +0200

    add constants

---
+++ constants.js
@@ -0,0 +1,15 @@
+// Constants used by the application
+
+const TOP = -60;
+const LEFT = -68;
+
+const WHITE = true;
+const BLACK = false;
+
+const TIME_TO_MOVE = 40;
+const FADE_TIME = 10;
+
+
+// Global Speed Controllers
+var CAMERA_TIME = 250;
+var SPEED_TIME = TIME_TO_MOVE;
